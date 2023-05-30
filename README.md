6c33db51e98a6349884b123f0fd106f090c3bd07
huyangsh <huyangshcn@gmail.com>
2023-05-30 15:23:23 -0400

    A clean merge.

--- README.md
+++
@@ -1,1 +0,0 @@
-# cooperation-in-games
--- game/__init__.py
+++
@@ -1,5 +0,0 @@
-from game.game import Game, Runner
-from game.utils import History
-
-from game.MatrixGame import MatrixGame
-from game.MonopolyGame import MonopolyGame, MonopolyTrajectoryRunner
